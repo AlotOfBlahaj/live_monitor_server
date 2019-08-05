cfdbd1b34b4f2c19e20ecf8ec52205745731a491
fzxiao233 <fzxiao@outlook.com>
2019-08-06 00:50:17 +0800

    [Bilibili]添加专栏检测功能

--- src/bilibili_api/bilibili_api.py
+++ src/bilibili_api/bilibili_api.py
@@ -2,10 +2,9 @@ from tools import get_json
 
 
 class BilibiliAPI:
-    @staticmethod
-    async def get_video_num(mid: int) -> int:
-        nav_info = await get_json(f'https://api.bilibili.com/x/space/navnum?mid={mid}&jsonp=jsonp')
-        video_num = nav_info['data']['video']
+    async def get_video_num(self, mid: int) -> int:
+        nav_info = await self.get_num_json(mid)
+        video_num = nav_info['video']
         return video_num
 
     @staticmethod
@@ -17,3 +16,21 @@ class BilibiliAPI:
         ref = f"https://www.bilibili.com/video/av{video['aid']}"
         return {'Title': title,
                 'Target': ref}
+
+    async def get_num_json(self, mid: int) -> dict:
+        nav_info = await get_json(f'https://api.bilibili.com/x/space/navnum?mid={mid}&jsonp=jsonp')
+        return nav_info['data']
+
+    async def get_article_num(self, mid: int) -> int:
+        nav_info = await self.get_num_json(mid)
+        article_num = nav_info['article']
+        return article_num
+
+    @staticmethod
+    async def get_article(mid: int) -> dict:
+        article_info = await get_json(f'https://api.bilibili.com/x/space/article?mid={mid}&jsonp=jsonp')
+        article = article_info['data']['articles'][0]
+        title = article['title']
+        target = f'https://www.bilibili.com/read/cv{article["id"]}'
+        return {'Title': title,
+                'Target': target}
